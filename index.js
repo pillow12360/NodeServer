870b62d6ee4d7e4f8ef5514ad5ffdb130700acfe
pillow12360 <ehdcks1256@naver.com>
2024-05-27 16:06:37 +0900

    프로젝트 초기 세팅

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json()); // Built-in middleware for JSON parsing
+app.use(express.urlencoded({ extended: true })); // Parsing URL-encoded data
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
