5e2477241a57b4a4d224ce6be0477e80d04ab24a
pillow12360 <ehdcks1256@naver.com>
2024-05-28 11:26:12 +0900

    스크립트 경로 수정

---
+++ dist/src/app.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
+const app = (0, express_1.default)();
+app.use(express_1.default.json()); // Built-in middleware for JSON parsing
+app.use(express_1.default.urlencoded({ extended: true })); // Parsing URL-encoded data
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(express_1.default.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
